4c5fb1bcde6002c7e89e1af4a180126b8232705d
thedinh20 <dinhthe12042004@gmail.com>
2025-01-07 11:30:26 +0700

    upload

--- App.js
+++ App.js
@@ -1,20 +1,100 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-export default function App() {
+const Box = ({ backgroundColor, text, time,title }) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.box, { backgroundColor }]}>
+      <Text style={styles.title}>{title}</Text>
+      <Text style={styles.text}>{text}</Text>
+      <Text style={styles.time}>{time}</Text>
     </View>
   );
-}
+};
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Box style={{
+        fontSize: 20,
+        fontWeight: '600',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop:40,
+      }}
+        title="Thông báo"
+      />
+
+      <Box 
+        backgroundColor='#c5d5e5'
+        title="Bước 1 Xác định nhu cầu khách hàng"
+        text="Vũ Văn Hoàng sắp hết hạn lúc 01/08/2020 9:00" 
+        time="20/08/2020, 6:00" 
+      />
+      <Box 
+        backgroundColor='#c5d5e5'
+        title="Bạn có khách hàng mới"
+        text="Chúc mừng bạn, bạn có khách hàng mới. Hãy mau chóng liên lạc ngay."
+        time="20/08/2020 6:00" 
+      />
+      <Box 
+        backgroundColor='#f1f8ff'
+        title="Khách hàng được chia sẻ bị trùng"
+        text="Rất tiếc, khách hàng được chia sẻ đã tồn tại trên hệ thống. Vui lòng chia sẻ khách hàng."
+        time="20/08/2020 6:00" 
+      />
+      <Box 
+        backgroundColor='#c5d5e5'
+        title="Khách hàng được thêm bị trùng"
+        text="Rất tiếc, khách hàng được chia sẻ đã tồn tại trên hệ thống. Vui lòng thêm khách hàng."
+        time="20/08/2020 6:00"
+      />
+      <Box 
+        backgroundColor='#f1f8ff'
+        title="Công việc sắp đến hạn trong hôm nay"
+        text="Bạn có 17 công việc sắp hết hạn trong hôm nay."
+        time="20/08/2020 6:00" 
+      />
+      <Box 
+        backgroundColor='#f1f8ff'
+        title="Công việc đã quá hạn"
+        text="Bạn có 17 công việc bị quá hạn. Hãy kiểm tra và lên kế hoạch hoàn thành công việc."
+        time="20/08/2020 6:00" 
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 40,
+    backgroundColor: '#eef',
     flex: 1,
-    backgroundColor: '#fff',
+    // justifyContent: 'center',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  box: {
+    
+    width: 415,
+    height: 100,
+    justifyContent: 'center'
+  },
+  title:{
+    marginLeft: 10,
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  text: {
+    marginTop: 5,
+    fontSize: 16,
+    marginLeft: 10,
+    color: 'black',
+  },
+  time: {
+    marginTop: 10,
+    fontSize:15,
+    marginLeft: 10,
+    color: 'gray',
+  }
 });
+
+export default App;
